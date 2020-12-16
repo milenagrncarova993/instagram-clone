f27dc04deca8b631884da1e778dedf793d1d1bb0
Milena Grncarova <milenagrncarov@outlook.com>
2020-12-16 15:47:08 +0100

    Posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   get "/dashboard" => "accounts#index"
+
+  resources :posts, only: [:new, :show, :create]
   root to: "public#homepage"
 end
